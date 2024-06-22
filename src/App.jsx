9e583933e673f81138c59cad780b91e3902b01e0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-06-23 02:26:03 +0600

    added setTodoValue(""); setTodoValue("");

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {
-  let todos = [
+
+
+  const [todos,setTodos]=useState([
     "Go to the gym",
     "Go to the park",
     "Go to the beach",
@@ -14,11 +17,15 @@ function App() {
     "Go to the supermarket",
     "Go to the bank",
     "Go to the post office",
-  ];
+  ])
+
+  const handleAddTodo=(newTodo)=>{
+    setTodos([...todos,newTodo])
+  }
 
   return (
     <>
-     <TodoInput/>
+     <TodoInput handleAddTodo={handleAddTodo}/>
      <TodoList todos={todos}/>
     </>
   )
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 
-export default function TodoInput() {
+export default function TodoInput({ handleAddTodo }) {
+  const [todoValue, setTodoValue] = useState("");
   return (
     <header>
-      <input type="text" placeholder="Enter todo...." />
-      <button>Add</button>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter todo...."
+      />
+      <button
+        onClick={() => {
+          handleAddTodo(todoValue);
+          setTodoValue("");
+        }}
+      >
+        Add
+      </button>
     </header>
   );
 }
