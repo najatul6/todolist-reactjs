d8dfdc6252b881fa49fd4f7fc7eceacef70c465a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-06-23 03:38:43 +0600

    add tast in localstorage and get and set items

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [todoValue, setTodoValue] = useState('');
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
 
-  const handleAddTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
-  };
-
-  const handleDeleteTodo = (index) => {
-   
-    setTodos(todos.filter((todo,todoIndex) => todoIndex!== index));
-  };
-
-  const handleUpdateTodo = (index) => {
-    setTodoValue(todos[index])
-    handleDeleteTodo(index);
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
   }
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+
+    console.log(localTodos)
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  }, [])
 
   return (
     <>
-      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodo={handleAddTodo} />
-      <TodoList handleDeleteTodo={handleDeleteTodo} handleUpdateTodo={handleUpdateTodo} todos={todos} />
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodo={handleAddTodos} />
+      <TodoList handleDeleteTodo={handleDeleteTodo} handleUpdateTodo={handleEditTodo} todos={todos} />
     </>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,18 @@
 import TodoCard from "./TodoCard";
 
 export default function TodoList(props) {
-    const {todos}=props
- 
-  return (
-    <ul className="main">
-      {todos?.map((todo, todoIndex) => {
-        return (
-          <TodoCard {...props} key={todoIndex} index={todoIndex}>
-            <p>{todo}</p>
-          </TodoCard>
-        );
-      })}
-    </ul>
-  );
+    const { todos } = props
+
+
+    return (
+        <ul className='main'>
+            {todos?.map((todo, todoIndex) => {
+                return (
+                    <TodoCard {...props} key={todoIndex} index={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
+                )
+            })}
+        </ul>
+    )
 }
