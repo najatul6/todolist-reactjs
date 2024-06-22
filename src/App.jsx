9e25c4f4248124043908c8f027df37d85f17769a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-06-23 01:37:36 +0600

    write custom css in index css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
 
 function App() {
   
 
   return (
-    <>
-     
-    </>
+    <main>
+     <TodoInput/>
+     <TodoList/>
+    </main>
   )
 }
 
